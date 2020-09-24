a9ce710049851044174adfe2b2e6111497b9260b
Guilherme Henrique <guilherme.almeida129@etec.sp.gov.br>
2020-09-24 19:03:16 -0300

    Exercício de fixação sobre matrizes

---
+++ courseFixation8/src/application/Program.java
@@ -0,0 +1,56 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		
+		System.out.print("Number of lines: ");
+		int m = sc.nextInt();
+		System.out.print("Number of columns: ");
+		int n = sc.nextInt();
+		
+		System.out.println();
+		
+		int mat[][] = new int[m][n];
+		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		
+		System.out.println();
+		
+		System.out.print("Type a number to search in the matrix: ");
+		int search = sc.nextInt();
+		
+		System.out.println();
+		
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				if (mat[i][j] == search) {
+					System.out.println("Position " + i + "," + j + ":");
+					if (j > 0) {
+						System.out.println("Left: " + mat[i][j-1]);
+					}
+					if (i > 0) {
+						System.out.println("Up: " + mat[i-1][j]);
+					}
+					if (j < mat[i].length-1) {
+						System.out.println("Right: " + mat[i][j+1]);
+					}
+					if (i < mat.length-1) {
+						System.out.println("Down: " + mat[i+1][j]);
+					}
+				}
+			}
+		}
+		
+		sc.close();
+	}
+
+}
